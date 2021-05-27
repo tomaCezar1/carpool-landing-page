675ef9c0997325dd3af9a5498765725ddba75cf6
tomaCezar1 <cezar.toma1@gmail.com>
2021-05-27 22:27:22 +0300

    Desktop version completed

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const passenger = document.querySelector('#passenger');
+const passenger = document.getElementById('passenger');
 const passengerBox = document.querySelector('.passenger-box');
-const passangerText = document.querySelector('#passanger-text');
-const driver = document.querySelector('#driver');
+const passangerText = document.getElementById('passanger-text');
+const driver = document.getElementById('driver');
 const driverBox = document.querySelector('.driver-box');
-const driverText = document.querySelector('#driver-text');
+const driverText = document.getElementById('driver-text');
+const howToText = document.querySelector('.how-to-text');
+const routeImg = document.querySelector('.route-container');
+const carsImg = document.querySelector('.how-to-img');
 
 const switchToDriver = () => {
   driver.classList.add('switched-driver');
   passenger.classList.remove('switch-on', 'switched-passenger');
   driverText.classList.add('bold-text');
   passangerText.classList.remove('bold-text');
+  howToText.innerHTML = 'Set your route, time, and available seats';
+  routeImg.style.display = 'block';
+  carsImg.style.display = 'none';
 };
 
 const switchToPassenger = () => {
@@ -17,6 +23,9 @@ const switchToPassenger = () => {
   driver.classList.remove('switch-on', 'switched-driver');
   passangerText.classList.add('bold-text');
   driverText.classList.remove('bold-text');
+  howToText.innerHTML = "See who's driving and ask to join";
+  routeImg.style.display = 'none';
+  carsImg.style.display = 'block';
 };
 
 passengerBox.addEventListener('click', switchToPassenger);
